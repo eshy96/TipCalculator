affaa25f7c003d25d9fb3d6037570644df892f7b
eshy96 <emathur360@gmail.com>
2016-09-19 20:04:49 -0700

    Updating Tip Calculator

---
+++ tippy/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Eshita Mathur on 9/13/16.
+//  Copyright © 2016 Eshita Mathur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        
+        
+        tipLabel.text = String(format: "S%.2f", tip)
+        totalLabel.text = String(format: "S%.2f", total)
+        
+    }
+
+}
+
